288e4ce99144a44911b91acf2e05301b9abcdffb
Uddesh <uddeshpatil2001@gmail.com>
2024-01-04 20:35:14 +0530

    [Uddesh] Developing a AddressBOOK

---
+++ _Address-Book_/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Address_Book_
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
---
+++ _Address-Book_/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using _Address_Book_;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("**************** Welcome to Address Book Program ****************");
+
+        Console.Write("\nEnter the first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter the last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter the address: ");
+        string address = Console.ReadLine();
+        Console.Write("Enter the city: ");
+        string city = Console.ReadLine();
+        Console.Write("Enter the state: ");
+        string state = Console.ReadLine();
+        Console.Write("Enter the ZIP: ");
+        int zip = int.Parse(Console.ReadLine());
+        Console.Write("Enter the phone number: ");
+        long phoneNumber = long.Parse(Console.ReadLine());
+        Console.Write("Enter the email: ");
+        string email = Console.ReadLine();
+
+       
+        Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        Console.WriteLine("\nContact created successfully!");
+    }
+}
