ecc979d4851b97378baeb30effd46d1de6451642
Uddesh <uddeshpatil2001@gmail.com>
2024-01-04 22:03:00 +0530

    [Uddesh] Modify program to add multiple person in address book

--- _Address-Book_/Program.cs
+++ _Address-Book_/Program.cs
@@ -9,10 +9,11 @@ class Program
         {
             Console.WriteLine("\nWhat can I do for you?");
             Console.WriteLine("1. Add a contact");
-            Console.WriteLine("2. Edit a contact");
-            Console.WriteLine("3. Delete a contact");
-            Console.WriteLine("4. View all contacts");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("2. Search for a contact");
+            Console.WriteLine("3. Edit a contact");
+            Console.WriteLine("4. Delete a contact");
+            Console.WriteLine("5. View all contacts");
+            Console.WriteLine("6. Exit");
 
             string choice = Console.ReadLine();
 
@@ -44,6 +45,30 @@ class Program
                     break;
 
                 case "2":
+                    Console.Write("\nEnter the first name of the contact: ");
+                    string _firstName = Console.ReadLine();
+                    Console.Write("Enter the last name of the contact: ");
+                    string _lastName = Console.ReadLine();
+
+                    Contact contact = addressBook.FindContactByName(_firstName, _lastName);
+                    if (contact != null)
+                    {
+                        Console.WriteLine("\nContact found:");
+                        Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                        Console.WriteLine($"Address: {contact.Address}");
+                        Console.WriteLine($"City: {contact.City}");
+                        Console.WriteLine($"State: {contact.State}");
+                        Console.WriteLine($"ZIP: {contact.ZipCode}");
+                        Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                        Console.WriteLine($"Email: {contact.Email}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nContact not found in the address book.");
+                    }
+                    break;
+
+                case "3":
                     Console.Write("\nEnter the first name of the contact to edit: ");
                     string firstNameToEdit = Console.ReadLine();
                     Console.Write("Enter the last name of the contact to edit: ");
@@ -69,7 +94,7 @@ class Program
                     addressBook.UpdateContact(firstNameToEdit, lastNameToEdit, newFirstName, newLastName, newAddress, newCity, newState, newZipCode, newPhoneNumber, newEmail);
                     break;
 
-                case "3":
+                case "4":
                     Console.Write("\nEnter the first name of the contact to delete: ");
                     string firstNameToDelete = Console.ReadLine();
                     Console.Write("Enter the last name of the contact to delete: ");
@@ -78,11 +103,11 @@ class Program
                     addressBook.DeleteContact(firstNameToDelete, lastNameToDelete);
                     break;
 
-                case "4":
+                case "5":
                     addressBook.DisplayAllContacts();
                     break;
 
-                case "5":
+                case "6":
                     Console.WriteLine("Exiting the program");
                     return;
 
--- _Address-Book_/obj/Debug/net8.0/_Address-Book_.AssemblyInfo.cs
+++ _Address-Book_/obj/Debug/net8.0/_Address-Book_.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_Address-Book_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a975fd429bcd468277031bfdf32b47b13f947e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572b70d43b1353012713adc66620aff9cff1c0d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("_Address-Book_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_Address-Book_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
